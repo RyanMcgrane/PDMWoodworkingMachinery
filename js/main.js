dd031ebf79b3fc50999b594ccd358191ba0a4120
Luka sMeiliunas <lukasmeiliunas@gmail.com>
2019-07-08 19:40:21 +0100

    index + css changes
    changed logo, removed content changed colors

--- js/main.js
+++ js/main.js
@@ -134,14 +134,8 @@ jQuery(document).ready(function( $ ) {
     }
   });
 
-  // Buy tickets select the ticket type on click
-  $('#buy-ticket-modal').on('show.bs.modal', function (event) {
-    var button = $(event.relatedTarget);
-    var ticketType = button.data('ticket-type');
-    var modal = $(this);
-    modal.find('#ticket-type').val(ticketType);
-  })
+  
 
-// custom code
+
 
 });
